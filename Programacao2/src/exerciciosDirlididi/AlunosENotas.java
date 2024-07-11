c840dfdda738104d23ba2ff96eaf193ebbb66679
lorenacarvalho <lonaca.paixao@gmail.com>
2024-07-11 18:54:21 -0300

    Update AlunoENotas.
    Readjustment for the use of Arrays only.

--- Programacao2/src/exerciciosDirlididi/AlunosENotas.java
+++ Programacao2/src/exerciciosDirlididi/AlunosENotas.java
@@ -6,57 +6,75 @@
 
 package exerciciosDirlididi;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class AlunosENotas {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		ArrayList <Integer> notas = new ArrayList<Integer>();
-		int maiorNota;
-		int menorNota;
-		int soma = 0;
-		int media = 0;
-		int qtdAcima = 0;
-		int qtdAbaixo = 0;
-		
-		while (true) {
-			
-			String[] linha = scanner.nextLine().split(" ");
-			if (linha[0].equals("-")) {
-				break;
-			}	
-			notas.add(Integer.parseInt(linha[1]));	
-			
-		}
-		
-		maiorNota = Collections.max(notas);
-		menorNota = Collections.min(notas);
-			
-		for (int nota: notas) {
-			soma += nota;
-			if (nota >= 700) {
-				qtdAcima += 1;
-			} else {
-				qtdAbaixo += 1;
-			}
-		}
-			
-		media = soma / notas.size();
-		
-		System.out.println("maior: " + maiorNota);
-		System.out.println("menor: " + menorNota);
-		System.out.println("media: " + media);
-		System.out.println("acima: " + qtdAcima);
-		System.out.println("abaixo: " + qtdAbaixo);
-		
-			
-		
+    	Scanner scanner = new Scanner(System.in);
+    	int[] notas;
+    	int maiorNota, menorNota;
+    	int soma = 0;
+    	int media = 0;
+    	int qtdAcima = 0;
+    	int qtdAbaixo = 0;
+    	String notasString = "";
 
-			
-			
-			
+    	while (true){
+        	String[] linha = scanner.nextLine().split(" ");
+        	if (linha[0].equals("-")){
+            	break;
+        	}
+
+        	notasString += " " + linha[1];
+
+    	}
+
+    	notasString = notasString.trim();
+    	String[] linhaNumeros = notasString.split(" ");
+    	notas = new int[linhaNumeros.length];
+
+    	for (int i = 0; i < linhaNumeros.length; i++){
+        	notas[i] = Integer.parseInt(linhaNumeros[i]);
+
+    	}
+
+    	for (int nota: notas) {
+        	soma += nota;
+        	if (nota >= 700) {
+            	qtdAcima += 1;
+        	} else {
+            	qtdAbaixo += 1;
+        	}
+    	}
+
+    	maiorNota = notas[0];
+    	menorNota = notas[0];
+
+    	for (int n: notas){
+        	if (n > maiorNota){
+            	maiorNota = n;
+        	}
+
+        	if (n < menorNota){
+            	menorNota = n;
+        	}
+
+    	}
+
+    	media = soma / notas.length;
+
+    	System.out.println("maior: " + maiorNota);
+    	System.out.println("menor: " + menorNota);
+    	System.out.println("media: " + media);
+    	System.out.println("acima: " + qtdAcima);
+    	System.out.println("abaixo: " + qtdAbaixo);
+
+
+
+
+
 	}
 }
+
